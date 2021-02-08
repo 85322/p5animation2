635258e4550208718a91f83367bbb942f9ea4a06
Anon853 <4m4zon3@web.de>
2021-02-08 18:17:14 +0100

    3rd

---
+++ single_sketch.js
@@ -0,0 +1,14 @@
+// Single-sketch example
+
+function setup (){
+  createCanvas (800, 600);
+}
+
+function draw(){
+  background(50);
+  fill(255);
+  noStroke();
+  //rectMode(CENTER);
+  //rect(mouseX, mouseY, 50, 50);
+  rect(70,50,50,50);
+}
