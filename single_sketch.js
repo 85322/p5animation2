1be895cbc2e9fb8bc058e36b43f3d31322fbdaba
Anon853 <51413744+Anon853@users.noreply.github.com>
2021-02-08 23:18:57 +0100

    Add files via upload

--- single_sketch.js
+++ single_sketch.js
@@ -1,14 +1,18 @@
-// Single-sketch example
-
 function setup (){
-  createCanvas (800, 600);
+  createCanvas (400, 400, WEBGL);
 }
 
 function draw(){
-  background(50);
+  background(0,0,0);
   fill(255);
   noStroke();
-  //rectMode(CENTER);
-  //rect(mouseX, mouseY, 50, 50);
-  rect(70,50,50,50);
+  fill(255, 150, 0)
+  
+  rect(50, 100, 50, 50);
+  circle(30, 350, 20);
+  ellipse(150, 100, 50, 25);
+  line(30, 20, 85, 75);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  box(50);
 }
