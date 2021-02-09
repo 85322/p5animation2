2441e96d6b2adaf6c5593e74abd9b990d3c17c39
Anon853 <51413744+Anon853@users.noreply.github.com>
2021-02-09 17:05:27 +0100

    moving

--- single_sketch.js
+++ single_sketch.js
@@ -1,18 +1,31 @@
+var moveRectX = 100;
+var posX = -200;
+
 function setup (){
   createCanvas (400, 400, WEBGL);
+  
 }
 
 function draw(){
-  background(0,0,0);
-  fill(255);
-  noStroke();
+  background(0,0,50);
+  strokeWeight(3, 204, 0);
+  stroke(50);
   fill(255, 150, 0)
-  
-  rect(50, 100, 50, 50);
-  circle(30, 350, 20);
+  circle(-100, 0, 20);
   ellipse(150, 100, 50, 25);
-  line(30, 20, 85, 75);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
+  line(-150, 150, 100, 0);
+
+  
+  rect(posX, -200, 25, 25);
+  posX+=1;
+  
+  if (posX > 170){
+    posX = -200;
+  }
+
+  rotateX(frameCount * 0.005);
+  rotateY(frameCount * 0.005);
   box(50);
+  fill(255, 0, 0);
+  rect(40, 40, 50, 50);
 }
