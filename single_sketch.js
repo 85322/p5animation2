d3afae454ad0f08d6369bf92e05c80305540d6e7
Anon853 <51413744+Anon853@users.noreply.github.com>
2021-02-09 22:06:21 +0100

    more animation logic

--- single_sketch.js
+++ single_sketch.js
@@ -9,7 +9,7 @@ let colors = {
 };
 
 let circle = {
-  x: -100,
+  x: -200,
   y: -150,
   diameter: 30
 };
@@ -20,8 +20,8 @@ function preload(){
   ExoBlack = loadFont('assets/Exo-Black.otf');
 }
 
-
-
+let x = 0;
+let speed = 3;
 
 function setup (){
   createCanvas (600, 400, WEBGL);
@@ -30,14 +30,27 @@ function setup (){
 }
 
 function draw(){
-  let bgc = mouseX;
-  background(bgc);
+  let b = 255;
+  let r = map(mouseX, 0, 600, 0, 255);
+  let g = map(mouseY, 0, 400, 0, 255);
+  
+  background(r, g, b);
   strokeWeight(3);
   stroke(50);
-  fill(255, 150, 0)
-  ellipse(150, 100, 50, 25);
-  line(-150, 150, 100, 0);
-  rect(mouseX, mouseY, 50, 50);
+  fill( 255, 150, 0 )
+  ellipse( 150, 100, 50, 25 );
+  line( -150, 150, 100, 0 );
+  rect( x, 120, 50, 50 );
+
+  if (x > 250) {
+    speed = -3;
+  } else if (x < -300) {
+    speed = +3;
+  }
+
+  x = x + speed;
+
+
   text("PosX: " + mouseX + "  PosY: "+ mouseY, -300, -185);
   fill (colors.red, colors.green, colors.blue);
   colors.red = Math.floor(Math.random()*255);
@@ -48,13 +61,13 @@ function draw(){
   posX++;
 
   if (posX > 175){
-    posX = -200;
+    posX = random(-300, width);
   }
 
   posY++;
  
   if (posY > 200){
-    posY = -200;
+    posY = random(-300, height);
   }
 
   size+=0.25;
@@ -64,14 +77,22 @@ function draw(){
   }
   
   ellipse(circle.x, circle.y, circle.diameter);
-
+  
+  circle.x++;
+   if (circle.x > width){
+     console.log("Ball off the screen!");
+     circle.x = -200;
+    
+   }
+   
+ 
   rotateX(frameCount * 0.005);
   rotateY(frameCount * 0.005);
   noFill();
   box(50);
 
   fill(255, 100, 10)
-  rect(40, 40, 50, 50);
+  rect(45, 40, 50, 50);
 
   
 }
