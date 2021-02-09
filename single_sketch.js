95dcbc6a4dd727a3718e2a460aa4241c1e49e974
Anon853 <51413744+Anon853@users.noreply.github.com>
2021-02-09 20:31:59 +0100

    Add files via upload

--- single_sketch.js
+++ single_sketch.js
@@ -1,31 +1,77 @@
 var moveRectX = 100;
-var posX = -200;
+let posX = -200;
+let posY = -190;
+let size = 25;
+let colors = { 
+  red: 0, 
+  green: 0, 
+  blue: 0 
+};
+
+let circle = {
+  x: -100,
+  y: -150,
+  diameter: 30
+};
+
+let myFont;
+
+function preload(){
+  ExoBlack = loadFont('assets/Exo-Black.otf');
+}
+
+
+
 
 function setup (){
-  createCanvas (400, 400, WEBGL);
-  
+  createCanvas (600, 400, WEBGL);
+  textSize(20);
+  textFont(ExoBlack);
 }
 
 function draw(){
-  background(0,0,50);
-  strokeWeight(3, 204, 0);
+  let bgc = mouseX;
+  background(bgc);
+  strokeWeight(3);
   stroke(50);
   fill(255, 150, 0)
-  circle(-100, 0, 20);
   ellipse(150, 100, 50, 25);
   line(-150, 150, 100, 0);
+  rect(mouseX, mouseY, 50, 50);
+  text("PosX: " + mouseX + "  PosY: "+ mouseY, -300, -185);
+  fill (colors.red, colors.green, colors.blue);
+  colors.red = Math.floor(Math.random()*255);
+  colors.green = Math.floor(Math.random()*255);
+  colors.blue = Math.floor(Math.random()*255);
 
-  
-  rect(posX, -200, 25, 25);
-  posX+=1;
-  
-  if (posX > 170){
+  rect(posX, posY, size, size);
+  posX++;
+
+  if (posX > 175){
     posX = -200;
   }
 
+  posY++;
+ 
+  if (posY > 200){
+    posY = -200;
+  }
+
+  size+=0.25;
+
+  if (size > 55) {
+    size = 25;
+  }
+  
+  ellipse(circle.x, circle.y, circle.diameter);
+
   rotateX(frameCount * 0.005);
   rotateY(frameCount * 0.005);
+  noFill();
   box(50);
-  fill(255, 0, 0);
+
+  fill(255, 100, 10)
   rect(40, 40, 50, 50);
+
+  
 }
